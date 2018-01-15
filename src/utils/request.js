01228c85b2397b30ce96df979bac04e2b94d407b
Daniel Arias <danielarias_123@msn.com>
2018-01-14 19:48:53 -0500

    Fix array url parsing

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,9 @@ const sanitize = (body = {}) => {
   Object.keys(sanitizedBody).forEach((field) => {
     if (typeof body[field] === 'object') {
       sanitizedBody[field] = sanitize(body[field]);
+      // Fix array issues
+      /* eslint-disable no-use-before-define */
+      if (body[field][0]) sanitizedBody[field] = objectToArray(body[field]);
     } else if (field === 'date') {
       // Fix date issues with JSON.stringify
       sanitizedBody[field] = new Date(body[field]);
@@ -10,6 +13,8 @@ const sanitize = (body = {}) => {
   });
   return sanitizedBody;
 };
+
+const objectToArray = (object = {}) => Object.keys(object).map(key => sanitize(object[key]));
 
 export {
   sanitize,
